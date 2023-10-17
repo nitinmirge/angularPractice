b20553483e0c5c7bdea412d83574907b2514a937
nitinmirge <nitinmirge123@gmail.com>
2023-10-17 14:23:00 +0530

    name validation update for multiwords in strings

--- angular/src/app/adminsignup/data.service.ts
+++ angular/src/app/adminsignup/data.service.ts
@@ -5,29 +5,30 @@ import { FormControl } from '@angular/forms';
   providedIn: 'root'
 })
 export class DataService {
-   city:string="Mumbai"
-   city2?:string;
+  city: string = "Mumbai"
+  city2?: string;
   constructor() { }
 
-     //Copy cOpy coPy copy copY
-     nameValidation(control:FormControl):any{
-      console.log("control",control.value);//copy
-      let nameValue = control.value;
-      let updateNewValue = nameValue?.toLowerCase();
-      let isInclude = updateNewValue?.includes('copy','hello','the');
-      return isInclude ? {isValid:true} : null;
-      }
-      //validation without valid input value
-      whiteSpaceValidator(inpFeildData:FormControl){
-        let nameValue = inpFeildData.value;
-        let trimedValue = nameValue.trim().length > 0;
-        return trimedValue ? null :{whiteSpace : true};
-      }
+  //Copy cOpy coPy copy copY
+  nameValidation(control: FormControl): any {
+    console.log("control", control.value);//copy
+    let nameValue = control.value;
+    let updateNewValue = nameValue?.toLowerCase();
+    // let isInclude = updateNewValue?.includes('copy');//For cheking single word only
+    let isInclude = updateNewValue?.includes('copy') || updateNewValue?.includes('heloo') || updateNewValue?.includes('nom')//For multiple words chekings
+    return isInclude ? { isValid: true } : null;
+  }
+  //validation without valid input value
+  whiteSpaceValidator(inpFeildData: FormControl) {
+    let nameValue = inpFeildData.value;
+    let trimedValue = nameValue.trim().length > 0;
+    return trimedValue ? null : { whiteSpace: true };
+  }
 
-      setCityName(value:any){
-          this.city2 = value;
-      }
-      getCityName(){
-         return this.city2
-      }
+  setCityName(value: any) {
+    this.city2 = value;
+  }
+  getCityName() {
+    return this.city2
+  }
 }
