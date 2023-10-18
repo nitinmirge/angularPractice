278e54e995ec75d5a526ab050ced8509f898ea62
nitinmirge <nitinmirge123@gmail.com>
2023-10-18 15:50:01 +0530

    directive ngClass

--- angular/src/app/directives/directives.component.ts
+++ angular/src/app/directives/directives.component.ts
@@ -16,13 +16,22 @@ export class DirectivesComponent {
   color?: string = 'orange'
   noColor:boolean = false;
   textColor = 'blue';
-  fontSize = '30px'
+  fontSize = '30px';
+  myName!:string;
+  firstName = 'raj'
+  headerContent = 'para';
+  headerContent1 = 'para1';
+  dynamicClass = false;
+  dynamicClass2 = false;
+  dynamicClass3 =true;
+
   constructor(private commonDataService:CommondataService){
 
   }
   //to fetch data from service u either go with onInit:if want to get or set data on page load or go with functions:if want to get or set data on events
   ngOnInit(){//Lifecycle hook : (fuction which implicitly get called by compiler ) used to fetch services properties
      this.gettingMarksOnInit = this.commonDataService.marks;
+    //  this.headerContent = 'para'
   }
   showContent(){
      this.show_content = false;
@@ -37,5 +46,8 @@ export class DirectivesComponent {
   switchDirective(){
     this.noColor=true
    this.color = 'blue'
+  }
+  dynamicCls(){
+    return 'para';
   }
 }
