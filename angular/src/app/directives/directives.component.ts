170d726c4d5d02659733679e502b0aabdb371566
nitinmirge <nitinmirge123@gmail.com>
2023-10-18 10:42:20 +0530

    commonservice created and ngSwitch directive

--- angular/src/app/directives/directives.component.ts
+++ angular/src/app/directives/directives.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CommondataService } from '../commondata.service';
 
 @Component({
   selector: 'app-directives',
@@ -10,14 +11,31 @@ export class DirectivesComponent {
   show_content : boolean = true;
   list = ['panir','kaju-kurma','icecream','veg kolhapuri','palak panir'];
   show_list: boolean = false;
+  gettingMarksOnClick?:number;
+  gettingMarksOnInit?:number;
+  color?: string = 'orange'
+  noColor:boolean = false;
+  textColor = 'blue';
+  fontSize = '30px'
+  constructor(private commonDataService:CommondataService){
 
+  }
+  //to fetch data from service u either go with onInit:if want to get or set data on page load or go with functions:if want to get or set data on events
+  ngOnInit(){//Lifecycle hook : (fuction which implicitly get called by compiler ) used to fetch services properties
+     this.gettingMarksOnInit = this.commonDataService.marks;
+  }
   showContent(){
      this.show_content = false;
   }
   showList(){
     this.show_list = true;
+    this.gettingMarksOnClick = this.commonDataService.marks;
   }
   hideList(){
     this.show_list = false;
+  }
+  switchDirective(){
+    this.noColor=true
+   this.color = 'blue'
   }
 }
